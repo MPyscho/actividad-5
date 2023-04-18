daac240c42cea495bc24f4e0d789177a39b37c0f
MPyscho <123724212+MPyscho@users.noreply.github.com>
2023-04-18 12:28:31 +0200

    hacer lo de for

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let nombreDeLaVariable = 'nombre de ususario';
 alert('¡Hola!');
 nombreDeLaVariable = prompt("introduzca un nombre");
 alert('Hola ' +nombreDeLaVariable)
+int i
+for(i=n;i<-1;i--)
+    {
+
+}
